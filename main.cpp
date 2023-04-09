6352a436e83e3c3e13860db8c65b8048dc3ac9c6
Nikolay Elshin <nikolay19830223@gmail.com>
2023-04-10 00:02:59 +0800

    error

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-using namespace std;
-
-const int ROWS = 3;
-const int COLS = 4;
-
-template <typename T> void Print(const T arr[], const int n);
-template <typename T> void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
+#include "asdfx.h"
+#include "constants.h"
+#include "Print.h"
 
 template <typename T> void FillRand(T arr[], const int n);
 void FillRand(double arr[], const int n);
@@ -509,25 +503,4 @@ void UniqueRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
 
 		}
 	}
-}
-
-template <typename T> void Print(const T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
-template <typename T> void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
 }
